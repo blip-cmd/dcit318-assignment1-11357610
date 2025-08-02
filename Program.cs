320abceecbca156fe94975580e736f1b082797a2
blip <ryanniiakweibrown1@gmail.com>
2025-08-02 20:22:37 +0000

    Task1: Add Grade Calculator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+//Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("WELCOME TO `RB GRADE CALCULATOR`");
+
+int grade;
+
+while(true)
+{
+    Console.Write("Enter a valid integer score: ");
+    string input = Console.ReadLine();
+    try
+    {
+        grade = int.Parse(input);
+        break;
+    }
+    catch (Exception)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid input. Please enter a valid integer score like '49'");
+        Console.ResetColor();
+    }
+}
+
+Console.WriteLine($"You entered {grade}.");
+Console.Write($"That is an ");
+if (grade >= 90) Console.WriteLine("A.");
+else if (grade >= 80) Console.WriteLine("B.");
+else if (grade >= 70) Console.WriteLine("C.");
+else if (grade >= 60) Console.WriteLine("D.");
+else Console.WriteLine("F.");
+
+Console.ResetColor();
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # dcit318-assignment1-11357610
+
+Grade Calculator 
+Write a program that prompts the user to enter a numerical grade between 0 and 100. Based 
+on the grade entered, display the corresponding letter grade using the following scale: - 
+90 and above: A - - - - 
+80-89: B 
+70-79: C 
+60-69: D 
+Below 60: F 
