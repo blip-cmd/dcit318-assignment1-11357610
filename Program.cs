0969ac63125fbd6af4d4b35f082572c8abbb7812
blip <ryanniiakweibrown1@gmail.com>
2025-08-02 20:38:59 +0000

    Task2: Add Ticket Price Calculator

--- Program.cs
+++ Program.cs
@@ -1,32 +1,42 @@
-﻿Console.ForegroundColor = ConsoleColor.Blue;
-//Console.BackgroundColor = ConsoleColor.DarkGreen;
-Console.WriteLine("WELCOME TO `RB GRADE CALCULATOR`");
+﻿//Console.ForegroundColor = ConsoleColor.Blue;
+////Console.BackgroundColor = ConsoleColor.DarkGreen;
+//Console.WriteLine("WELCOME TO `GRADE CALCULATOR(RB)`");
 
-int grade;
+//int grade;
 
-while(true)
-{
-    Console.Write("Enter a valid integer score: ");
-    string input = Console.ReadLine();
-    try
-    {
-        grade = int.Parse(input);
-        break;
-    }
-    catch (Exception)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Invalid input. Please enter a valid integer score like '49'");
-        Console.ResetColor();
-    }
-}
+//while(true)
+//{
+//    Console.Write("Enter a valid integer score: ");
+//    string input = Console.ReadLine();
+//    try
+//    {
+//        grade = int.Parse(input);
+//        break;
+//    }
+//    catch (Exception)
+//    {
+//        Console.ForegroundColor = ConsoleColor.Red;
+//        Console.WriteLine("Invalid input. Please enter a valid integer score like '49'");
+//        Console.ResetColor();
+//    }
+//}
 
-Console.WriteLine($"You entered {grade}.");
-Console.Write($"That is an ");
-if (grade >= 90) Console.WriteLine("A.");
-else if (grade >= 80) Console.WriteLine("B.");
-else if (grade >= 70) Console.WriteLine("C.");
-else if (grade >= 60) Console.WriteLine("D.");
-else Console.WriteLine("F.");
+//Console.WriteLine($"You entered {grade}.");
+//Console.Write($"That is an ");
+//if (grade >= 90) Console.WriteLine("A.");
+//else if (grade >= 80) Console.WriteLine("B.");
+//else if (grade >= 70) Console.WriteLine("C.");
+//else if (grade >= 60) Console.WriteLine("D.");
+//else Console.WriteLine("F.");
 
-Console.ResetColor();
+//Console.ResetColor();
+//// END OF GRADE CALCULATOR
+///
+
+
+Console.WriteLine("WELCOME TO `Ticket Price Calculator (RB)`");
+Console.Write("Enter the age: ");
+int age = int.Parse(Console.ReadLine());
+int price = (age <= 12 || age>= 65 ? 7 : 10);
+Console.WriteLine($"Ticket Price: GHC {price}.00");
+Console.WriteLine("END OF `Ticket Price Calculator (RB)`");
--- README.md
+++ README.md
@@ -8,3 +8,10 @@ on the grade entered, display the corresponding letter grade using the following
 70-79: C 
 60-69: D 
 Below 60: F 
+
+
+Ticket Price Calculator 
+A movie theater sells tickets for GHC10. However, if a customer is a senior citizen (age 65 
+and above) or a child (age 12 and below), they receive a discounted price of GHC7. Write a 
+program that prompts the user to enter their age and calculates the ticket price based on the 
+age entered. Display the ticket price accordingly. 
